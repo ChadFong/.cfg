fd943603b43babbbeb4e5ff6ee1198e08347e05e
Chad Fong <chadfong@Chads-MacBook-Pro.local>
2020-10-26 18:02:02 -0700

    Convert to `fish` shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+alias cfg '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+alias vimrc "vim ~/.vimrc"
+
+alias prof "vim ~/.config/fish/config.fish"
+alias runprof ". ~/.config/fish/config.fish"
+
+alias g git
+
